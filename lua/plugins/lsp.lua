46eeeb1e0752502c2e0e0683e32abdd308ea1b8e
reecekalmar <ralmarkalmar@gmail.com>
2024-09-04 13:47:14 -0600

    added csharp plugin and cleaned up a little

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,12 +23,36 @@ return {
 
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            { "ms-jpq/coq_nvim",       branch = "coq" },
+
+            -- 9000+ Snippets
+            { "ms-jpq/coq.artifacts",  branch = "artifacts" },
+
+            -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+            -- Need to **configure separately**
+            { "ms-jpq/coq.thirdparty", branch = "3p" },
+            -- - shell repl
+            -- - nvim lua api
+            -- - scientific calculator
+            -- - comment banner
+            -- - etc
+        },
+
+        init = function()
+            vim.g.coq_settings = {
+                auto_start = true, -- if you want to start COQ at startup
+                -- Your COQ settings here
+            }
+        end,
+
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.texlab.setup({})
-            lspconfig.omnisharp.setup({
-                cmd = { "dotnet", "/home/reece311/.config/omnisharp/OmniSharp.dll" },
+            --[[          lspconfig.omnisharp.setup({
+                cmd = { "dotnet", "/home/reece311/.config/omnisharp/OmniShardp.dll" },
                 settings = {
                     FormattingOptions = {
                         EnableEditorConfigSupport = true,
@@ -53,6 +77,8 @@ return {
                     ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
                 },
             })
+            ]]
+            --
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
