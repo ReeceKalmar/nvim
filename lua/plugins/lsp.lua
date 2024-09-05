d0968bce3f7a2369bc0fa5efd3fe1b5c3d6549bd
reecekalmar <ralmarkalmar@gmail.com>
2024-09-05 07:01:40 -0600

    not dealing with csharp.nvim lol

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,93 +1,91 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗",
-                    },
-                },
-            })
-        end,
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({ automatic_installation = true })
-        end,
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({ automatic_installation = true })
+		end,
+	},
 
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        dependencies = {
-            { "ms-jpq/coq_nvim",       branch = "coq" },
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		dependencies = {
+			{ "ms-jpq/coq_nvim", branch = "coq" },
 
-            -- 9000+ Snippets
-            { "ms-jpq/coq.artifacts",  branch = "artifacts" },
+			-- 9000+ Snippets
+			{ "ms-jpq/coq.artifacts", branch = "artifacts" },
 
-            -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-            -- Need to **configure separately**
-            { "ms-jpq/coq.thirdparty", branch = "3p" },
-            -- - shell repl
-            -- - nvim lua api
-            -- - scientific calculator
-            -- - comment banner
-            -- - etc
-        },
+			-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
+			-- Need to **configure separately**
+			{ "ms-jpq/coq.thirdparty", branch = "3p" },
+			-- - shell repl
+			-- - nvim lua api
+			-- - scientific calculator
+			-- - comment banner
+			-- - etc
+		},
 
-        init = function()
-            vim.g.coq_settings = {
-                auto_start = true, -- if you want to start COQ at startup
-                -- Your COQ settings here
-            }
-        end,
+		init = function()
+			vim.g.coq_settings = {
+				auto_start = true, -- if you want to start COQ at startup
+				-- Your COQ settings here
+			}
+		end,
 
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.texlab.setup({})
-            --[[          lspconfig.omnisharp.setup({
-                cmd = { "dotnet", "/home/reece311/.config/omnisharp/OmniShardp.dll" },
-                settings = {
-                    FormattingOptions = {
-                        EnableEditorConfigSupport = true,
-                        OrganizeImports = nil,
-                    },
-                    MsBuild = {
-                        LoadProjectsOnDemand = nil,
-                    },
-                    RoslynExtensionsOptions = {
-                        EnableAnalyzersSupport = nil,
-                        EnableImportCompletion = nil,
-                        AnalyzeOpenDocumentsOnly = nil,
-                    },
-                    Sdk = {
-                        IncludePrereleases = true,
-                    },
-                },
-                handler = {
-                    ["textDocument/definition"] = require("omnisharp_extended").definition_handler,
-                    ["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
-                    ["textDocument/references"] = require("omnisharp_extended").references_handler,
-                    ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
-                },
-            })
-            ]]
-            --
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.texlab.setup({})
+			lspconfig.omnisharp.setup({
+				cmd = { "dotnet", "/home/reece311/.config/omnisharp/OmniShardp.dll" },
+				settings = {
+					FormattingOptions = {
+						EnableEditorConfigSupport = true,
+						OrganizeImports = nil,
+					},
+					MsBuild = {
+						LoadProjectsOnDemand = nil,
+					},
+					RoslynExtensionsOptions = {
+						EnableAnalyzersSupport = nil,
+						EnableImportCompletion = nil,
+						AnalyzeOpenDocumentsOnly = nil,
+					},
+					Sdk = {
+						IncludePrereleases = true,
+					},
+				},
+				handler = {
+					["textDocument/definition"] = require("omnisharp_extended").definition_handler,
+					["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
+					["textDocument/references"] = require("omnisharp_extended").references_handler,
+					["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
-        end,
-    },
-    {
-        "Hoffs/omnisharp-extended-lsp.nvim",
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+		end,
+	},
+	{
+		"Hoffs/omnisharp-extended-lsp.nvim",
+	},
 }
