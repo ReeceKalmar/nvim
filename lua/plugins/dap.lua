c5e6691b3d78302fce408d51e7c7c0d7f7914f0c
ReeceKalmar <ralmarkalmar@gmail.com>
2024-09-19 17:38:29 -0600

    k

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -109,16 +109,6 @@ return {
 					max_value_lines = 100, -- Can be integer or nil.
 				},
 			})
-
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
 		end,
 	},
 }
